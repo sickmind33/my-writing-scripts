372b113eb1aa62a0970ef047e173c5b7eede1774
sickmind33 <sickmind33@gmail.com>
2020-10-12 18:34:11 -0400

    backported bugfix
    0 feet 0 inches bugfix backported

--- charactermaker.py
+++ charactermaker.py
@@ -2,10 +2,11 @@
 import random
 letters = ("b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z")
 vowels = ("a","e","i","o","u","y",)
-creatures = ("dog","cat","horse","dragon","fox","tiger","donkey","rhino","elephant","lion","cheetah","lizard","raccoon","mouse","gazelle","giraffe","zebra")
+creatures = ("dog","cat","horse","dragon","fox","tiger","donkey","rhino","elephant","deer","lion","cheetah","lizard","raccoon","mouse","gazelle","giraffe","zebra")
 gender = ("male","female")
 boobsize = ("small","medium","large","massive")
-emotions = ("happy","sad","angry","jealous","dominating","submissive","horny")
+dicksize = ("small","medium","large","massive")
+emotions = ("happy","sad","angry","jealous","dominating","submissive","horny","hungry","tired","exhausted","excited")
 
 species = str()
 name = str()
@@ -16,8 +17,9 @@ sag = str()
 finalName = str()
 feet = int()
 inches = int()
-height = int()
+height = str()
 breasts = str()
+dick = str()
 attitude = str()
 
 length = random.randint(2,3)
@@ -26,13 +28,18 @@ while length != count:
     name = name + glyph
     glyph = vowels[random.randint(0,5)]
     name = name + glyph
-    species = creatures[random.randint(0,16)]
+    species = creatures[random.randint(0,17)]
     sag = gender[random.randint(0,1)]
     feet = random.randint(0,8)
-    inches = random.randint(0,11)
-    attitude = emotions[random.randint(0,6)]
+    if feet == 0:
+        inches = random.randint(1,11)
+    else:
+        inches = random.randint(0,11)
+    attitude = emotions[random.randint(0,10)]
     if sag == "female":
         breasts = boobsize[random.randint(0,3)]
+    else:
+        dick = dicksize[random.randint(0,3)]
     count = count + 1
     
 
@@ -44,4 +51,6 @@ print(finalName)
 print("Attitude:",attitude)
 if sag == "female":
     print("breast size:",breasts)
-print(feet,"feet and ",inches,"inches tall")
+else:
+    print("penis size:",dick)
+print(feet,"feet and",inches,"inches tall")
